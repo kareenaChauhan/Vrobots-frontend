36e6ff66f734b9f6debaebde2191463c051fc55a
Ritik-Vrozart <ritik@vrozart.com>
2025-07-18 13:45:54 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -19,9 +19,9 @@ export default defineConfig({
     },
   },
   server: {
-    port: "3000",
+    port: 3000,
     host: "0.0.0.0",
     strictPort: true,
-    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
+    // allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
   }
 });
